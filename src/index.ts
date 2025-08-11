2a43b059a0ef935ce6b55555b3f046d65954bf2d
fgorsky <fedor@support-pc.org>
2025-08-11 14:33:03 +0300

    fix exports

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import {Event} from "./models/event";
 import {Role} from "./models/role";
 import {Participant} from "./models/participant";
 import {Resource} from "./models/resource";
+import SectionPlace from "./models/section-place";
 import retryFetch from "./utils/retry-fetch";
 import serverFetch from "./utils/server-fetch";
 import {authOptions} from "./auth/option";
@@ -13,6 +14,7 @@ export {
     Event,
     Role,
     Participant,
+    SectionPlace,
     Resource,
     retryFetch,
     serverFetch,
